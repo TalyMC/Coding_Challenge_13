a18dc1134b8744dfe3d4eedd75aa9d7087539d80
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-03-09 20:36:42 -0400

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,1 +1,40 @@
 //Task 2  Adding Employee Cards Dynamically
+function createElement(name, position) {
+    const employeeCard = document.createElement ('div');
+    employeeCard.setAttribute('class', 'employee-card');
+    
+    
+    //Employee name as the heading
+    const heading = document.createElement('h3');
+    heading.textContent = name; 
+    
+    //Employee Position
+    const paragraph = document.createElement('h4');
+    paragraph.textContent = position;
+
+    //Remove Button
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'abolish';
+    removeButton.addEventListener('click',() => {
+        employeeCard.remove();
+    })
+
+    //Edit Button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+
+    //Appending Elements
+    employeeCard.appendChild(heading);
+    employeeCard.appendChild(paragraph);
+    employeeCard.appendChild(removeButton);
+    employeeCard.appendChild(editButton);
+
+    document.getElementById('employeeContainer').appendChild(employeeCard)
+}
+
+//Employee Example
+createElement('Alberto', 'Finance Manager');
+createElement('Marvin', 'Accounting');
+createElement('Enrique', 'Marketing Director');
+
+//Task 2 
